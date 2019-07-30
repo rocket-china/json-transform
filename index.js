69390a383fb9ff3c1cc0d0649b05c9db6cdf3e06
Jhaidi <1753612833@qq.com>
2019-07-30 22:31:23 +0800

    write file

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ fs.readFile(file, 'utf-8', function(err, data) {
         console.log('文件读取失败');
     } else {
         var obj = JSON.parse(data);
+        var writeObj =  {};
         for (var key in obj) {
             if (obj.hasOwnProperty(key)) {
                 var element = obj[key];
                 console.log(element);
+               writeObj[key] = element + 'a';
             }
         }
-        console.log(data);
+        var writeJson = JSON.stringify(writeObj, null, 4);
+        fs.writeFile('transform.json', writeJson, function(err) {
+            if (err)  throw err;
+            console.log('done');
+        });
     }
 });
 
